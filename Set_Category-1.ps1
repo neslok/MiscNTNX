7e40d56e0c88f65734aecb178774b8417be04ea1
Keith Olsen <keith.r.olsen@gmail.com>
2022-03-31 21:20:05 -0600

    update

--- Set_Category-1.ps1
+++ Set_Category-1.ps1
@@ -3,9 +3,9 @@
 # Update with information specific to your environment #
 ########################################################
 
-$prismcentIP = "10.42.156.40"
+$prismcentIP = "192.168.1.1"
 $RESTAPIUser = "admin"
-$RESTAPIPassword = "nx2Tech156!"
+$RESTAPIPassword = "Password"
 $VMuuid = "6fd8fce2-0e9a-429c-9270-0bb6758dda66"
 
 
@@ -24,16 +24,19 @@ $headers.Add("Authorization", "Basic "+[System.Convert]::ToBase64String([System.
 
 $VMspec = Invoke-WebRequest -SkipCertificateCheck $BaseURL'vms/'$VMuuid -Method 'GET' -Headers $headers
 
-# Update Category - NOTE: assumes current categories is blank
+# Update Category - NOTE: assumes current categories is blank - and is updated with Category Name and Value
 
 $VMspec = $VMspec.Content.Replace('"categories": {}', '"categories": {"KOTEST": "KOTEST1"}') | ConvertFrom-Json
 
+# Create Spec and Metadata sections from $VMspec array
 
 $NewSpec = $VMspec.spec | ConvertTo-Json -depth 15
 $Newmeta = $VMspec.metadata | ConvertTo-Json -depth 15
 
+# Create body for VM update 
+
 $body = '{"spec":' + $NewSpec + "," + '"metadata": ' + $Newmeta + '}'
-Write-Host $body
+#Write-Host $body
 
 # Update VM 
 
